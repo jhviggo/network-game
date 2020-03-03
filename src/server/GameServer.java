29745bd5a1935b9d69a709fe5b81d0ebf045456c
Kasper <leisznern@gmail.com>
2020-03-03 10:32:28 +0100

    Adds new players in list on server

--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -1,11 +1,14 @@
 package server;
 
+import game.Player;
 import interfaces.GameThread;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GameServer {
@@ -13,6 +16,7 @@ public class GameServer {
     private static boolean SERVER_RUNNING = true;
     private static Set<GameServerThread> connections = new HashSet<>();
     private static GameServerHandle gameServerHandle = new GameServerHandle();
+    private static List<Player> playerList = new ArrayList<>();
 
     public static void main(final String[] args) throws IOException {
         final ServerSocket socket = new ServerSocket(PORT);
@@ -38,9 +42,15 @@ public class GameServer {
     }
 
     public static void relay(GameThread connection, String message) {
+        String[] messageItems = message.split(" ");
+
         for (GameServerThread thread : connections) {
             if (thread.getSocket().isConnected()) {
                 try {
+                    if (messageItems[0] == "ADDPLAYER") {
+                        playerList.add(new Player(messageItems[1], Integer.parseInt(messageItems[2]), Integer.parseInt(messageItems[3]), "UP"));
+                    }
+
                     thread.send(message);
                 } catch (Exception e) {
                     System.out.println(e);
